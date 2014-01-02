57579c598c6ed7ec03e0ef7878df69a761a005e2
cloud <190296465@qq.com>
2014-01-02 18:13:22 +0800

    2014.01.02

--- Lib/Controller.php
+++ Lib/Controller.php
@@ -12,14 +12,6 @@ class Controller {
         echo $error_id;
     }
 
-    //检查是否登录，如果已登录，返回登录用户的信息
-    function check_login() {
-        $cuserLogin = new Login();
-        $this->user = $cuserLogin->getUser();
-        if (!isset($this->user->id))
-            header('Location:/login/login');
-    }
-
 }
 
 ?>
