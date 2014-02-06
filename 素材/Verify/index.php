255923ea9d4a71a031ec1a40a28bece485e42030
cloud <190296465@qq.com>
2014-02-06 18:00:20 +0800

    0206 1800 code

--- 素材/Verify/index.php
+++ 素材/Verify/index.php
@@ -1,6 +1,4 @@
 <?php
-
-//setcookie('Uname', $_POST['Uname']);
 $array = array();
 $Domain = $_POST['Domain'];
 $Code = $_POST['Code'];
@@ -11,7 +9,7 @@ if (in_array("{$Code}:{$Domain}", $StringArray)) {
     $array['INFO'] = '您已通过商业授权！';
 } else {
     $array['Y'] = 0;
-    $array['INFO'] = '您还不是正版授权用户！';
+    $array['INFO'] = '<p style="width:500px; margin:200px auto; padding:20px; border:1px solid #A0522D; border-radius:10px; color:#A0522D; background:#FFF; font: 30px/60px Microsoft Yahei; ">您的授权已经终止!请与管理员联系.联系QQ:190296465</p>';
 }
 echo json_encode($array);
 ?>
