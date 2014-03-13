15bf0529dc67e6eef9044f1cdf06adfbc7f2e407
cloud <190296465@qq.com>
2014-03-13 17:09:00 +0800

    2014.3.13

--- CMS_TCIT/Cms/Tpl/user/login.php
+++ CMS_TCIT/Cms/Tpl/user/login.php
@@ -1,31 +1,21 @@
 <link type="text/css" href="/Static/style/login.css" rel="stylesheet" />
-<div class="login">
-	<div class="login_box">
-		<div class="login_content">
-			<input id="userID" name="userID" type="text" class="username"  onfocus="if(this.value=='请输入用户名'){this.value=''};this.style='color:#333' " onblur="if(this.value==''){this.value='请输入用户名';this.style='color:#999'}" />
-			<div id="pwd">
-				<input type="password" class="pwd" id="passWord" name="passWord"  onfocus="if(this.value=='请输入密码'){this.value=''};this.style='color:#333' " onblur="if(this.value==''){this.value='请输入密码';this.style='color:#999'}" onkeydown="if(event.keyCode==13) ajaxLogin();" />
-			</div>
-			<!--消息提示  控制P标签-->
+<div class="header"> <a href="javascript:void(0);"><img src="/Static/images/login_logo.png"></a> </div>
+<div class="main">
+	<div class="login_picture">
+		<div class="login_box">
+			<input name="userID" id="userID" type="text" placeholder="请输入用户名" >
+			<input name="passWord" id="passWord" type="passWord" placeholder="请输入密码" onkeydown="if(event.keyCode==13) sublogin();">
 			<div class="infos_tips"></div>
-			<div class="input_remember">
-				<input type="checkbox" name="remember" id="remember" class="remember" />
-				记住账号</div>
-			<a href="javascript:void(0);" class="login_btn">登录</a> <span> 忘记密码? 点我 <a rel="nofollow" target="_top" href="tencent://message/?uin=327110323"><img src="http://wpa.qq.com/pa?p=2:327110323:41" style="vertical-align:middle;" /></a></span> </div>
+			<a href="javascript:void(0);" title="登录" class="login_btn">登 录</a> <span> <a href="/Welcome/reg/cname/0" class="login_reg" title="免费注册" target="_blank">免费注册</a> | <a href="/Forgetpsw/index" title="忘记密码" target="_blank">忘记密码</a> | <a href="/Public/help/help_index.html" title="帮助中心" target="_blank">帮助中心</a> </span> </div>
 	</div>
-	<div class="login_info">
-		<div class="login_int">
-			<ul>
-				<li> <i class="class_icon"></i> <font>远景目标</font> <em>做最专业的网络营销创意服务及网络技术服务支持平台！</em> </li>
-				<li> <i class="time_icon"></i> <font>我们的使命</font> <em>让信息化和电子商务实现得更简单!</em> </li>
-				<li> <i class="zidong_icon"></i> <font>价值观</font> <em>客户至上、诚信、道德<br />创新、激情敬业 </em> </li>
-			</ul>
-		</div>
-	</div>
-	<!-- footer -->
-	<div class="login_footer"> <a href="http://www.evller.com/a/lianxiwomen/2013/0318/28.html">关于我们</a> | <a href="http://www.evller.com/a/lianxiwomen/">联系我们</a> | <a href="http://www.evller.com/a/lianxiwomen/2013/0314/25.html">支付方式</a>| <a href="http://www.evller.com/a/400dianhua/">400电话</a><br>
-		Copyright © 2002 - 2014 成都易维恒信科技有限公司 蜀ICP备08005485号 </div>
-	<!-- footer -->
+</div>
+<div class="login_text">
+	<ul>
+		<span><em>全天候 全地域</em>全天候全地域实时在线，<br />
+		自动查询商标申请进展和状态。</span> <span class="text02"><em>商标异常100%预警</em>商标异常我来管，<br />
+		邮件、站内双重预警。</span> <span class="text03"><em>官方数据 严谨如一</em>状态监测、实时跟踪，<br />
+		所有信息同步国家商标数据库。</span>
+	</ul>
 </div>
 <script type="text/javascript">
 $(document).ready(function () {
@@ -49,7 +39,7 @@ function ajaxLogin(){
 			if (data == 1) {
 				location.href = "/admin.php";
 			} else {
-				$(".infos_tips").html('<p><i class="tips_icon"></i>' + data + '</p>');
+				$(".infos_tips").html('<p class="tips_icon">' + data + '</p>');
 			}
 		}
 	});
