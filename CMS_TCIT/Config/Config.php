46c995f6f4f9887b4de133487c6e69a034ba4ac0
cloud <190296465@qq.com>
2014-03-20 10:45:37 +0800

    3.20

--- CMS_TCIT/Config/Config.php
+++ CMS_TCIT/Config/Config.php
@@ -13,12 +13,12 @@ return array(
     'EMAIL_CONFIG' => array(
         'SMTP_HOST' => 'smtp.qq.com', //SMTP服务器
         'SMTP_PORT' => '465', //SMTP服务器端口
-        'SMTP_USER' => '', //SMTP服务器用户名
-        'SMTP_PASS' => '', //SMTP服务器密码
-        'FROM_EMAIL' => '', //发件人EMAIL
-        'FROM_NAME' => '', //发件人名称
-        'REPLY_EMAIL' => '', //回复EMAIL（留空则为发件人EMAIL）
-        'REPLY_NAME' => '', //回复名称（留空则为发件人名称）
+        'SMTP_USER' => '190296465@qq.com', //SMTP服务器用户名
+        'SMTP_PASS' => 'nys312', //SMTP服务器密码
+        'FROM_EMAIL' => '190296465@qq.com', //发件人EMAIL
+        'FROM_NAME' => 'cloud', //发件人名称
+        'REPLY_EMAIL' => '190296465@qq.com', //回复EMAIL（留空则为发件人EMAIL）
+        'REPLY_NAME' => 'cloud', //回复名称（留空则为发件人名称）
     ),
     'NOTES' => array(
         '现在是:' . date('Y年m月d日 H:i:s') => 'javascript:;',
